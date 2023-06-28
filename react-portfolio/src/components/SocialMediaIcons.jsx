9104c1f7bb66b72abfd1b86ed640063d63445062
fbuonojr <fbuonojr@fordham.edu>
2023-06-27 23:59:38 -0400

    social media icon component

---
+++ react-portfolio/src/components/SocialMediaIcons.jsx
@@ -0,0 +1,41 @@
+
+const SocialMediaIcons = () => {
+    return(
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com/in/frank-buono-812160147/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alr="linkedin-link" src="../assets/linkedin.png" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com/in/frank-buono-812160147/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alr="linkedin-link" src="../assets/linkedin.png" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com/in/frank-buono-812160147/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alr="linkedin-link" src="../assets/linkedin.png" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com/in/frank-buono-812160147/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alr="linkedin-link" src="../assets/linkedin.png" />
+            </a>
+        </div>
+    )
+}
+
+export default SocialMediaIcons;
--- react-portfolio/src/scenes/Landing.jsx
+++ react-portfolio/src/scenes/Landing.jsx
@@ -1,4 +1,4 @@
-//import SocialMediaIcons from "../components/SocialMediaIcons";
+import SocialMediaIcons from "../components/SocialMediaIcons";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
@@ -93,6 +93,20 @@ const Landing = ({ setSelectedPage }) => {
                         Let's talk
                     </div>
                 </AnchorLink>
+            </motion.div>
+
+            <motion.div
+              className="flex mt-5 justify-center md:justify-start"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2,duration: 0.5 }}
+              variants={{
+                  hidden: { opacity: 0, x: -50 },
+                  visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <SocialMediaIcons />
             </motion.div>
         </div>
 
