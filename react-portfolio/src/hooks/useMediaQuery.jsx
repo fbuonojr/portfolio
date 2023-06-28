82f693a3ccb590016abbe463e9e32b55cf898280
fbuonojr <fbuonojr@fordham.edu>
2023-06-27 20:31:44 -0400

    navbar in app

--- react-portfolio/src/hooks/useMediaQuery.jsx
+++ react-portfolio/src/hooks/useMediaQuery.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 const useMediaQuery = (query) => {
-    const [matches, setMatchers] = useState(false);
+    const [matches, setMatches] = useState(false);
 
     useEffect(() => {
         const media = window.matchMedia(query);
         if(media.matches !== matches){
-            setMatchers(media.matches);
+            setMatches(media.matches);
         }
-        const listener = () => setMatchers(media.matches);
+        const listener = () => setMatches(media.matches);
         window.addEventListener("resize", listener);
         return () => window.removeEventListener("resize", listener);
     }, [matches, query]);
