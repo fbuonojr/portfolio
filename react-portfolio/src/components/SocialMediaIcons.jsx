1e65817289dc09382f0ee3edccebf25c5457ef3d
frankbuonojr <frankbuonojr@gmail.com>
2025-04-06 15:58:05 -0400

    removed unnecessary social icons

--- react-portfolio/src/components/SocialMediaIcons.jsx
+++ react-portfolio/src/components/SocialMediaIcons.jsx
@@ -10,30 +10,6 @@ const SocialMediaIcons = () => {
             >
                 <img alr="linkedin-link" src="../assets/linkedin.png" />
             </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://www.linkedin.com/in/frank-buono-812160147/"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img alr="linkedin-link" src="../assets/linkedin.png" />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://www.linkedin.com/in/frank-buono-812160147/"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img alr="linkedin-link" src="../assets/linkedin.png" />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://www.linkedin.com/in/frank-buono-812160147/"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img alr="linkedin-link" src="../assets/linkedin.png" />
-            </a>
         </div>
     )
 }
